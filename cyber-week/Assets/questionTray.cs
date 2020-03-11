ca97b57ecf6173be45c0bc3f6b32e9d4265defbe
Dany Lewin <danyglewin@gmail.com>
2020-03-11 17:24:57 +0200

    added question tab; need to add mouse support

---
+++ cyber-week/Assets/questionTray.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class questionTray : MonoBehaviour
+{
+
+    [SerializeField] GameObject tab;
+    [SerializeField] GameObject body;
+
+    [SerializeField] bool isOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOpen = false;
+        BoxCollider2D collider = gameObject.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            toggleOpen();
+        }
+    }
+
+    void toggleOpen()
+    {
+        toggleActive(tab);
+        toggleActive(body);
+    }
+
+    void OnMouseDown() {
+        toggleOpen();
+    }
+
+    void toggleActive(GameObject gameObject)
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
