c589e2d8d8c01c4ee1e4fcc25edc3c39a02a9ff9
unknown <ofir485@gmail.com>
2020-03-17 17:26:14 +0200

    level5+random questions

---
+++ cyber-week/Assets/scripts/projectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+   
+    [SerializeField] Vector3 movementvector= new Vector3(0.1f,0,0); // to understand = new Vector3(0, 60f, 0);
+    Vector3 startingPos;
+    [SerializeField] float xBorder=-30f; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + movementvector*Time.deltaTime;
+        if (transform.position.x <= xBorder);
+        {
+            print(transform.position);
+            transform.position = startingPos;
+        }
+
+    }
+}
---
+++ cyber-week/Assets/scripts/questionTray.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+// using System.Web.Script.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class questionTray : MonoBehaviour
+{
+
+    [SerializeField] GameObject tab;
+    [SerializeField] GameObject body;
+
+    [SerializeField] Text questionBox;
+    [SerializeField] Text[] optionBoxes;
+
+    [SerializeField] GameObject[] platforms;
+
+    Question question;
+    int[] answerOrder;
+
+    string tagRight = "right_answer";
+    string tagWrong = "wrong_answer";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadQuestionsToObject();
+        randomiseOrder();
+        applyQuestionText();
+        applyPlatformTags();
+    }
+
+    void loadQuestionsToObject()
+    {
+        string text = System.IO.File.ReadAllText("questions.json");
+        print(text);
+        // var serializer = new JavaScriptSerializer();
+        string questionText = "Second question";
+        string rightAnswer = "Apple";
+        string[] allAnswers = new string[] { "Apple", "Pineapple", "Orange", "Papaya" };
+        question = new Question(questionText, rightAnswer, allAnswers);
+    }
+
+    void applyQuestionText()
+    {
+        questionBox.text = question.questionText;
+        for (int i = 0; i < 4; i++)
+        {
+            optionBoxes[i].text = question.allAnswers[answerOrder[i]];
+        }
+
+    }
+
+    void randomiseOrder()
+    {
+        List<int> MyArray = new List<int> { 0, 1, 2, 3 };
+        System.Random rnd = new System.Random();
+        int[] MyRandomArray = MyArray.OrderBy(x => rnd.Next()).ToArray();
+        answerOrder = MyRandomArray;
+    }
+
+    void applyPlatformTags()
+    {
+        foreach (GameObject platform in platforms)
+        {
+            platform.tag = tagWrong;
+        }
+        int rightAnswerIndex = Array.Find<int>(answerOrder, i => answerOrder[i] == 0);
+        platforms[rightAnswerIndex].tag = tagRight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            toggleOpen();
+        }
+    }
+
+    public void toggleOpen()
+    {
+
+        toggleActive(tab);
+        toggleActive(body);
+    }
+
+    void toggleActive(GameObject gameObject)
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+    private class Question
+    {
+        public string questionText;
+        public string rightAnswer;
+        public string[] allAnswers;
+
+        public Question(string questionText, string rightAnswer, string[] allAnswers)
+        {
+            this.questionText = questionText;
+            this.rightAnswer = rightAnswer;
+            this.allAnswers = allAnswers;
+        }
+    }
+
+}
